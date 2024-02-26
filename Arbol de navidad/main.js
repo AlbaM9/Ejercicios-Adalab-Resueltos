c2283e6390fa9d842e4137c357d6a0b97875491e
Alba Melchor Gomez <albamelchorgomez@gmail.com>
2024-02-26 09:40:21 +0100

    tree

---
+++ Arbol de navidad/main.js
@@ -0,0 +1,38 @@
+
+const numRows = prompt('Enter the number of rows:');
+const rowsImput = parseInt(numRows);
+
+const trianglePattern = generateTrianglePattern(rowsImput);
+console.log(trianglePattern);
+
+
+function generateTrianglePattern(rows) {
+    let pattern = '';
+    for (let i = 0; i <= rows; i++) {
+
+        if (i == 0) {
+            pattern += ' '.repeat(rows - i * 2 - 1);
+            pattern += '★';
+
+        } else if (i == rows) {
+
+            pattern += ' '.repeat(rows * 1 - 1);
+            pattern += "|";
+        }
+        else {
+
+            pattern += ' '.repeat(rows - i);
+
+            for (let j = 0; j < i * 2 - 1; j++) {
+
+                pattern += '▲';
+            }
+        }
+        pattern += '\n';
+
+    }
+    return pattern;
+
+}
+
+
